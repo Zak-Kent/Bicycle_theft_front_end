4233eafda4f711cb4e8e75f8759f55c9489c649a
Zachary Kent <zak.kent@gmail.com>
2016-08-08 15:10:08 -0700

    got draggable marker and rack search hitting url working

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,15 +7,63 @@
  * # MainCtrl
  * Controller of the angularTheftAppApp
  */
+
+// !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! issues below !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! 
+// still have issue with pagination and returning more than 25 rack per get request 
+// need to make option of placing marker 
+// create button to redo search with new marker location 
+
 angular.module('angularTheftAppApp')
+    .constant('baseURL','http://localhost:8000/api/v1/racks/')
+    .controller('MapCtrl', ['$scope', '$http', 'baseURL', function($scope, $http, baseURL) {
 
-  .controller('MapCtrl', function($scope, rackFinder) {
+        $scope.map = { center: { latitude: 46, longitude: -123 }, zoom: 8 };
 
-    $scope.map = { center: { latitude: 46, longitude: -123 }, zoom: 8 };
+        // work on moving url call to a factory service 
+        // $scope.markers = rackFinder;
+// ----------------------------------------------------------------------------------------------
+        $scope.marker = {
+          id: 0,
+          coords: {
+            latitude: 45.521570,
+            longitude: -122.673371
+          },
+          options: { draggable: true },
+          events: {
+            dragend: function (marker) {
+              console.log('marker dragend');
+              var lat = marker.getPosition().lat();
+              var lon = marker.getPosition().lng();
+              console.log(lat);
+              console.log(lon);
 
-    $scope.markers = rackFinder.query();
-    console.log($scope.markers);
+              $scope.marker.options = {
+                draggable: true,
+                labelContent: 'lat: ' + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                labelAnchor: '100 0',
+                labelClass: 'marker-labels'
+              };
+            }
+          }
+        };
 
-  })
-  ;
+
+// ----------------------------------------------------------------------------------------------
+        $scope.markerBreakdown = [];
+
+        $http.get(baseURL+'?dist=100&point=-122.678713,45.514798')
+        .then(function(response) {
+            $scope.markers = response.data.results; 
+            // break up json object to format gmap can use with markers 
+            for (var i = 0; i < $scope.markers.length; i++) { 
+                var obj = {id: $scope.markers[i].id, 
+                        longitude: $scope.markers[i].geom.coordinates[0],
+                        latitude: $scope.markers[i].geom.coordinates[1]};
+
+                $scope.markerBreakdown.push(obj);
+            }
+        });
+
+
+    }]);
 
--- app/scripts/services/data.js
+++ app/scripts/services/data.js
@@ -1,10 +1,26 @@
 'use strict';
 
 angular.module('angularTheftAppApp')
-.factory('rackFinder', function($resource){
-    return $resource('http://localhost:8000/api/v1/racks/?dist=200&point=-122.678713,45.514798');
+.factory('rackFinder', ['$http', 'baseURL', function($http, baseURL){
 
-});
+    $http.get(baseURL+'?dist=100&point=-122.678713,45.514798')
+    .then(function(response) {
+        var markers = response.data.results; 
 
-// below is command that looks at local resource for rack data. maybe use in testing 
-// return $resource('json_data/json_data.json');
+        console.log(markers);
+        console.log(markers.length);
+
+        var markerBreakdown = [];
+
+        // break up json object to format gmap can use with markers 
+        for (var i = 0; i < markers.length; i++) { 
+            var obj = {id: markers[i].id, 
+                    longitude: markers[i].geom.coordinates[0],
+                    latitude: markers[i].geom.coordinates[1]};
+
+            markerBreakdown.push(obj);
+        }
+    });
+
+}]);
+
